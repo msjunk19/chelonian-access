38a71d047ab786c2bf5f1c4f9b2f7ecacbf11b17
Macchiavelli Spielberg <msjunk19@gmail.com>
2026-03-21 00:35:33 -0400

    master_uid management started, writes master uids to eeprom

--- lib/uid_manager/master_uid_manager.h
+++ lib/uid_manager/master_uid_manager.h
@@ -1,112 +1,145 @@
 #include <EEPROM.h>
 
-#define MAX_MASTERS 3
-#define UID_LEN 7
-#define EEPROM_START 0
-#define OBFUSCATION_KEY 0xAA
+#define EEPROM_SIZE 128   // Total EEPROM size to use
 
 class MasterUIDManager {
 public:
-    MasterUIDManager() {
-        EEPROM.begin(MAX_MASTERS * UID_LEN + MAX_MASTERS);
-        loadFromEEPROM();
+    MasterUIDManager(uint16_t eepromSize = EEPROM_SIZE) {
+        EEPROM.begin(eepromSize);
     }
 
-    // Check if a UID is master
-    bool isMaster(const uint8_t* uid, uint8_t length) {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            if (masterLengths[i] == length) {
+    void writeUIDs(uint8_t** uids, uint8_t* lengths, size_t count) {
+        uint16_t addr = findFirstFreeAddress();
+
+        for (size_t i = 0; i < count; i++) {
+            uint8_t len = lengths[i];
+
+            if (addr + 1 + len > EEPROM_SIZE) {
+                Serial.println("Not enough EEPROM space to append all UIDs!");
+                break;
+            }
+
+            if (checkUID(uids[i], len)) {
+                Serial.print("UID already exists, skipping: ");
+                printUID(uids[i], len);
+                continue;
+            }
+
+            EEPROM.write(addr++, len);
+            for (int j = 0; j < len; j++) {
+                EEPROM.write(addr++, uids[i][j]);
+            }
+        }
+
+        EEPROM.commit();
+        Serial.println("UIDs written/appended to EEPROM!");
+    }
+
+    void readUIDs() {
+        uint16_t addr = 0;
+        int index = 0;
+
+        while (addr < EEPROM_SIZE) {
+            uint8_t len = EEPROM.read(addr++);
+            if (len == 0xFF || len == 0) break;
+
+            Serial.print("UID #");
+            Serial.print(index++);
+            Serial.print(" (length ");
+            Serial.print(len);
+            Serial.print("): ");
+
+            for (int i = 0; i < len; i++) {
+                Serial.print(EEPROM.read(addr++), HEX);
+                Serial.print(" ");
+            }
+            Serial.println();
+        }
+    }
+
+    bool checkUID(uint8_t* uid, uint8_t len) {
+        uint16_t addr = 0;
+
+        while (addr < EEPROM_SIZE) {
+            uint8_t storedLen = EEPROM.read(addr++);
+            if (storedLen == 0xFF || storedLen == 0) break;
+
+            if (storedLen == len) {
                 bool match = true;
-                for (int j = 0; j < length; j++) {
-                    if ((masterUIDs[i][j] ^ OBFUSCATION_KEY) != uid[j]) {
+                for (int i = 0; i < len; i++) {
+                    if (EEPROM.read(addr + i) != uid[i]) {
                         match = false;
                         break;
                     }
                 }
                 if (match) return true;
             }
+            addr += storedLen;
         }
+
         return false;
     }
 
-    // Add a master UID manually or via first-boot learning
-    bool addMaster(const uint8_t* uid, uint8_t length) {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            if (masterLengths[i] == 0) { // empty slot
-                masterLengths[i] = length;
-                for (int j = 0; j < length; j++) {
-                    masterUIDs[i][j] = uid[j] ^ OBFUSCATION_KEY;
+    // Remove a UID from EEPROM
+    bool removeUID(uint8_t* uid, uint8_t len) {
+        uint16_t addr = 0;
+
+        while (addr < EEPROM_SIZE) {
+            uint16_t uidStart = addr;
+            uint8_t storedLen = EEPROM.read(addr++);
+            if (storedLen == 0xFF || storedLen == 0) break;
+
+            bool match = (storedLen == len);
+            for (int i = 0; i < len && match; i++) {
+                if (EEPROM.read(addr + i) != uid[i]) {
+                    match = false;
                 }
-                saveToEEPROM();
+            }
+
+            if (match) {
+                // Shift all subsequent UIDs left to overwrite this one
+                uint16_t nextAddr = addr + storedLen;
+                while (nextAddr < EEPROM_SIZE) {
+                    uint8_t b = EEPROM.read(nextAddr);
+                    EEPROM.write(addr, b);
+                    addr++;
+                    nextAddr++;
+                }
+
+                // Clear remaining space at the end
+                while (addr < EEPROM_SIZE) {
+                    EEPROM.write(addr++, 0xFF);
+                }
+
+                EEPROM.commit();
+                Serial.print("UID removed: ");
+                printUID(uid, len);
                 return true;
             }
-        }
-        return false; // no free slot
-    }
 
-    // Remove a master UID
-    bool removeMaster(const uint8_t* uid, uint8_t length) {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            if (masterLengths[i] == length) {
-                bool match = true;
-                for (int j = 0; j < length; j++) {
-                    if ((masterUIDs[i][j] ^ OBFUSCATION_KEY) != uid[j]) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
-                    masterLengths[i] = 0;
-                    saveToEEPROM();
-                    return true;
-                }
-            }
+            addr += storedLen; // move to next UID
         }
-        return false;
-    }
 
-    // First boot / learning mode
-    bool firstBootLearning(const uint8_t* uid, uint8_t length) {
-        if (noMasters()) {
-            addMaster(uid, length);
-            return true; // indicate learning happened
-        }
+        Serial.println("UID not found to remove.");
         return false;
     }
 
 private:
-    uint8_t masterUIDs[MAX_MASTERS][UID_LEN] = {0};
-    uint8_t masterLengths[MAX_MASTERS] = {0};
-
-    void saveToEEPROM() {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            int addr = EEPROM_START + i * UID_LEN;
-            for (int j = 0; j < UID_LEN; j++) {
-                EEPROM.write(addr + j, masterUIDs[i][j]);
-            }
+    uint16_t findFirstFreeAddress() {
+        uint16_t addr = 0;
+        while (addr < EEPROM_SIZE) {
+            uint8_t len = EEPROM.read(addr);
+            if (len == 0xFF || len == 0) break;
+            addr += 1 + len;
         }
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            EEPROM.write(EEPROM_START + MAX_MASTERS * UID_LEN + i, masterLengths[i]);
-        }
-        EEPROM.commit();
+        return addr;
     }
 
-    void loadFromEEPROM() {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            int addr = EEPROM_START + i * UID_LEN;
-            for (int j = 0; j < UID_LEN; j++) {
-                masterUIDs[i][j] = EEPROM.read(addr + j);
-            }
+    void printUID(uint8_t* uid, uint8_t len) {
+        for (int i = 0; i < len; i++) {
+            Serial.print(uid[i], HEX);
+            Serial.print(" ");
         }
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            masterLengths[i] = EEPROM.read(EEPROM_START + MAX_MASTERS * UID_LEN + i);
-        }
-    }
-
-    bool noMasters() {
-        for (int i = 0; i < MAX_MASTERS; i++) {
-            if (masterLengths[i] != 0) return false;
-        }
-        return true;
+        Serial.println();
     }
 };
