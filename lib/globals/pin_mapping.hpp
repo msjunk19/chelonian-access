4bab873be489ecdbeacac84a3012881705d459aa
Macchiavelli Spielberg <msjunk19@gmail.com>
2026-03-21 22:03:15 -0400

    formatting change

--- lib/globals/pin_mapping.hpp
+++ lib/globals/pin_mapping.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+//==== Map ALL Pins here for ease of use ====//
 
-// Pins for Internal/Single Solor External LED (8) and Neopixel (10)
-#define PN_LED (8)
-#define PN_NEOPIXEL (10)
+// Relay Pin Mappings
+#define RELAY_1 (0)
+#define RELAY_2 (1)
+#define RELAY_3 (2)
+#define RELAY_4 (3)
 
 // ESP32-C3 SPI pins for PN532
 #define PN532_SCK (4)
@@ -11,12 +14,10 @@
 #define PN532_MOSI (6)
 #define PN532_SS (7)
 
+// Pins for Internal/Single Solor External LED (8) and Neopixel (10)
+#define PN_LED (8)
+#define PN_NEOPIXEL (10)
+
 // RX and TX Pins for JQ6500
 #define RX_PIN (20)
 #define TX_PIN (21)
-
-// Relay Pin Mappings
-#define RELAY_1 (0)
-#define RELAY_2 (1)
-#define RELAY_3 (2)
-#define RELAY_4 (3)
